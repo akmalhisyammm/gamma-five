9ad7c41396a7cf3fa04321ae74088db6173be2b4
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2021-11-12 21:29:07 +0700

    feat: add form in kontak page

--- src/pages/kontak.tsx
+++ src/pages/kontak.tsx
@@ -1,10 +1,60 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Grid,
+  Heading,
+  Input,
+  Textarea,
+} from '@chakra-ui/react';
 import { NextPage } from 'next';
+
 import Layout from 'components/layout';
 
 const Kontak: NextPage = () => {
   return (
     <Layout title="Kontak">
-      <h1>Hello Kontak!</h1>
+      <Heading textAlign="center">Kontak Kami</Heading>
+      <hr
+        style={{
+          border: '1px solid',
+          width: 100,
+          margin: '12px auto 0',
+        }}
+      />
+
+      <Box marginY={12}>
+        <Grid gridTemplateColumns={['1fr', 'repeat(2, 1fr)']} gap={4}>
+          <FormControl id="firstname" isRequired>
+            <FormLabel>Nama Depan</FormLabel>
+            <Input type="text" />
+          </FormControl>
+          <FormControl id="lastname" isRequired>
+            <FormLabel>Nama Belakang</FormLabel>
+            <Input type="text" />
+          </FormControl>
+        </Grid>
+
+        <FormControl id="email" marginTop={6} isRequired>
+          <FormLabel>Email</FormLabel>
+          <Input type="email" />
+        </FormControl>
+
+        <FormControl id="subject" marginTop={6} isRequired>
+          <FormLabel>Subjek</FormLabel>
+          <Input type="text" />
+        </FormControl>
+
+        <FormControl id="message" marginTop={6} isRequired>
+          <FormLabel>Pesan</FormLabel>
+          <Textarea placeholder="Masukkan pesan" />
+        </FormControl>
+
+        <Button colorScheme="teal" type="submit" marginY={6}>
+          Kirim
+        </Button>
+      </Box>
     </Layout>
   );
 };
